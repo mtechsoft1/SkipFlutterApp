296d05c6ca12eb795ea3a9ca4466acb6c6ead925
Ayesha Amjad <ashamjad007@gmail.com>
2024-05-06 10:38:01 +0500

    Initial commit

---
+++ lib/splash_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Container(
+            decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage('assets/images/background-image.png'),
+                    fit: BoxFit.cover))));
+  }
+}
